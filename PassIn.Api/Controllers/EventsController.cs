ac91ef402f4dc87dd84f767da20aaa33afebd1de
babingthon <babingthon.bandeira@gmail.com>
2024-04-06 20:17:59 -0300

    feat: aula 1

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PassIn.Api.UseCases.Events.GetById;
+using PassIn.Api.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using System.Linq.Expressions;
+
+namespace PassIn.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredEventJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public IActionResult Register([FromBody] RequestEventJson request)
+    {
+
+        try
+        {
+            var UseCase = new RegisterEventUseCase();
+
+            var response = UseCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch (PassInException ex)
+        {
+            return BadRequest(new ResponseErrorJson(ex.Message));
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unkwown error."));
+        } 
+   
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(ResponseEventJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    public IActionResult GetById([FromRoute] Guid id)
+    {
+        try
+        {
+            var useCase = new GetEventByIdUseCase();
+
+            var response = useCase.Execute(id);
+
+            return Ok(response);
+        }
+        catch (PassInException ex)
+        {
+            return NotFound(new ResponseErrorJson(ex.Message));
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unkwown error."));
+        }
+
+    }
+}
---
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
+using System;
+
+namespace PassIn.Infrastructure;
+public class PassInDbContext : DbContext
+{   
+    public DbSet<Event> Events { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\Babingthon\\Desktop\\PassIn\\PassInDb.db");
+    }
+  
+}
